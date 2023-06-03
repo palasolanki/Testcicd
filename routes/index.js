971e925495076679eafc9aa36dbe7ccf4e196094
pala solanki <pala.radicalloop@gmail.com>
2023-06-03 19:38:13 +0530

    Test2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Anand Test1' });
+  res.render('index', { title: 'Anand Test5' });
 });
 
 module.exports = router;
